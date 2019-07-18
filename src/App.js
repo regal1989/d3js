d9916f0a41e652d4092c7c1547bae9739a8ae65e
regal.singh <Regal.Singh@rci.rogers.com>
2019-07-19 01:38:42 +0530

    table render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BarChart from './BarChart'
+import TableRender from './TableRender'
 
 function App() {
   return (
     <div className="App">
-      <BarChart data={[5,10,1,3]} size={[500,500]} />
+      <TableRender/>
     </div>
   );
 }
 
 export default App;
+
+// <BarChart data={[5,10,1,3]} size={[500,500]} />
---
+++ src/TableRender.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import './App.css'
+class TableRender extends Component {
+    constructor() {
+        super();
+    
+        this.state = {
+          data : [
+            {id : 1, status : "Shipped"},
+            {id : 2, status : "Not Shipped"},
+          ],
+          expandedRow: null,
+        };
+      }
+
+   handleRowClick(rowId) {
+        const currentExpandedRow = this.state.expandedRow;
+        this.setState({ expandedRow: (rowId === currentExpandedRow) ? null : rowId })
+    }
+
+    renderItem(item) {
+        const clickCallback = () => this.handleRowClick(item.id);
+
+        const itemRows = [
+          <tr key={"row-data-" + item.id}>
+            <td onClick={clickCallback} key={"row-data-" + item.id}>{item.id}</td>
+           </tr>
+         ];
+    
+        if (this.state.expandedRow === item.id) {
+          itemRows.push(
+            <tr>
+                <td colspan={Object.keys(this.state.data[0]).length}>
+                  <div> {item.status} </div>
+                </td>
+            </tr>
+          );
+        }
+    
+        return itemRows;    
+    }
+    render() {
+        let allItemRows = [];
+        this.state.data.forEach(item => {
+            const perItemRows = this.renderItem(item);
+            allItemRows = allItemRows.concat(perItemRows);
+        });
+        return (
+			     <table>{allItemRows}</table>
+        );
+    }
+}
+export default TableRender
