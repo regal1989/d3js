d9a42d8bdf56cd3350475894bbc721f1914a1db3
regal.singh <Regal.Singh@rci.rogers.com>
2019-07-21 19:46:55 +0530

    'bar-line-pie'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import BarChart from './BarChart'
-import TableRender from './TableRender'
+import LineChart from './LineChart'
+import PieClass from "./PieClass";
+import RBarChart from "./RBarChart";
+import * as d3 from "d3";
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.changechart = this.changechart.bind(this)
+    this.state = { chartvalue : "Pie" };
+  }
+
+  changechart = function(event){
+    this.setState({chartvalue: event.target.value});
+  }
+
+  render() {
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+
+  const data = generateData();
+
+  const linewidth = 500, lineheight = 350, linemargin = 20
+  const Linedata = [
+    {a: 1, b: 3},
+    {a: 2, b: 6},
+    {a: 3, b: 2},
+    {a: 4, b: 12},
+    {a: 5, b: 8}
+  ]
+
   return (
     <div className="App">
-      <BarChart data={[5,10,1,3]} size={[500,500]} />
+      <div>
+               <select id="chart" onChange={this.changechart} value={this.state.chartvalue}>
+                  <option value="select">Select</option>
+                  <option value="Pie">Pie</option>
+                  <option value="RBar">Bar</option>
+                  <option value="Line">Line</option>
+               </select>
+               <p></p>
+               <p>{this.state.chartvalue}</p>
+      </div>
+      <div className={(this.state.chartvalue==='Pie' ? 'showbox' : 'hiddenbox')}>
+        <PieClass
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+      <div className={(this.state.chartvalue==='RBar' ? 'showbox' : 'hiddenbox')}>
+        <RBarChart />
+      </div>
+      <div id="linePage" className={(this.state.chartvalue==='Line' ? 'showbox' : 'hiddenbox')}>
+        <LineChart data={Linedata} width={linewidth} height={lineheight} margin={linemargin}/>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/PieChart.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import './App.css'
+class PieChart extends Component {
+
+}
+
+
+export default PieChart
